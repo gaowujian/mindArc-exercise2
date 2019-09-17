3af7476300385517d6d3856564966386a371779e
Tony Gao <gwjlshebei@gmail.com>
2019-09-17 23:12:08 +1000

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
+import Tabs from "react-bootstrap/Tabs"
+import Tab from "react-bootstrap/Tab"
+import Accordion from "react-bootstrap/Accordion"
+import Card from "react-bootstrap/Card"
+import MediaQuery from "react-responsive"
+import "./App.css"
+class App extends React.Component {
+  state = {
+    isFetched: false,
+    data: []
+  }
+  componentDidMount() {
+    this.getUsers()
+  }
+  getUsers = async () => {
+    const response = await axios.get("./data.json")
+    const data = response.data
+    this.setState({ isFetched: true, data: data })
+  }
+  render() {
+    const { isFetched, data } = this.state
+    return (
+      <div className="App">
+        {isFetched && (
+          <Tabs defaultActiveKey={"Section 1"} className="Tabs">
+            {data.map((item, index) => {
+              const htmlString = item.content
+              return (
+                <Tab
+                  key={index}
+                  eventKey={item.title}
+                  title={item.title}
+                  className="Tab"
+                  dangerouslySetInnerHTML={{ __html: htmlString }}
+                ></Tab>
+              )
+            })}
+          </Tabs>
+        )}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {isFetched && (
+          <Accordion defaultActiveKey="0" className="Accordion">
+            {data.map((item, index) => {
+              const htmlString = item.content
+              return (
+                <Card key={index}>
+                  <Accordion.Toggle as={Card.Header} variant="link" eventKey={index}>
+                    {item.title}
+                  </Accordion.Toggle>
+
+                  <Accordion.Collapse eventKey={index}>
+                    <Card.Body dangerouslySetInnerHTML={{ __html: htmlString }}></Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              )
+            })}
+          </Accordion>
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
